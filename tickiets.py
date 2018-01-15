be2bde562ab3e0ad9578e77a45b68a7807700be8
shangpf1 <208418427@qq.com>
2018-01-15 22:12:19 +0800

    tickiets.py

--- tickiets.py
+++ tickiets.py
@@ -23,15 +23,31 @@ from stations import stations
 class Train_tickiets(object):
     def printTrainInfo(self):
         arguments = docopt(__doc__)
-        print(arguments['<date>'],arguments['<from>'],arguments['<to>'])
+        print(arguments['<date>'],arguments['<from>'],arguments['<from>'])
         date = arguments['<date>']
         fromstation = stations.get(arguments['<from>'])
         tostation = stations.get(arguments['<to>'])
-        
-        url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)
+        url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={]&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)
+        #url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)
         r = requests.get(url)
         print(url)
         print(r.json())
+        allresults = r.json()
+        allTickets = allresults['data']['result']
+        print(len(allTickets),allTickets)
+        rows= self.parse_train(allTickets)
+        print(rows)
+
+    def parse_train(self,allTickets):
+        rows = []
+        for ticket in allTickets:
+            trainlist = ticket.split('|')
+            trainrow=[]
+            trainrow.extend(trainlist[3:6])
+            trainrow.extend(trainlist[8:11])
+            trainrow.extend(trainlist[32:21:-1])
+            rows.append(trainrow)
+        return rows
 
 
 if __name__ == '_main_':
