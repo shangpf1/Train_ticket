a8f6f529b2167f9d75d73d93e16be3a76c76c667
shangpf1 <208418427@qq.com>
2018-01-17 22:43:55 +0800

    init project

---
+++ ticket.py
@@ -0,0 +1,33 @@
+"""Train tickets query via command-line.
+Usage:
+   tickets [-gdtkz] <from> <to> <date>
+Options:
+   -h,--help   显示帮助菜单
+   -g          高铁
+   -d          动车
+   -t          特快
+   -k          快速
+   -z          直达
+Example:
+   tickets 上海 北京 2017-12-05
+"""
+
+from docopt import docopt
+import requests
+from stations import stations
+
+class Tickiets(object):
+    def printTrainInfo(self):
+        arguments = docopt(__doc__)
+        print(arguments['<date>'],arguments['<from>'],arguments['<from>'])
+        date = arguments['<date>']
+        fromstation = stations.get(arguments['<from>'])
+        tostation = stations.get(arguments['<to>'])
+                
+        url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)
+        #url = 'https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date,fromstation,tostation)        r = requests.get(url)
+        print(url)
+        print(r.json())
+    
+if __name__ == '__main__':
+    Tickiets().printTrainInfo()
